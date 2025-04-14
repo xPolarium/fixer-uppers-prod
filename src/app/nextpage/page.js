29ec506e4350238c705355f56685453ee7153cdc
Edwin Serna <sernaedwin0@gmail.com>
2025-04-14 18:38:38 -0500

    Initalization

---
+++ src/app/nextpage/page.js
@@ -0,0 +1,11 @@
+// Index page *****
+import Image from "next/image";
+
+export default function newPage() {
+  
+  return (
+    <>
+      <h1 style={{color: "white"}}>hello page</h1>
+    </>
+  );
+}
