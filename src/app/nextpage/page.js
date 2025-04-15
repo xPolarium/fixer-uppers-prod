afb952bce88ed12dd0c90004fd8e24fff406292e
Edwin Serna <sernaedwin0@gmail.com>
2025-04-14 20:30:00 -0500

    Component example created and call from idex

--- src/app/nextpage/page.js
+++ src/app/nextpage/page.js
@@ -1,11 +1,8 @@
-// Index page *****
+// src/app/nextpage/page.js
 import Image from "next/image";
-
-export default function newPage() {
-  
+// USing PascelCasing for page names
+export default function NewPage() {
   return (
-    <>
-      <h1 style={{color: "white"}}>hello page</h1>
-    </>
+      <h1 style={{ color: "teal", backgroundColor: "white"}}>hello page</h1>
   );
 }
