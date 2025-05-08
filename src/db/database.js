a5713a915b7648f9ad61a295c5834d7dc0ed8eab
Polarium <mikeajr@icloud>
2025-05-08 17:55:24 -0500

    Added more tables

--- src/db/database.js
+++ src/db/database.js
@@ -5,9 +5,8 @@ import Database from "better-sqlite3";
 
 const db = new Database("sqlite.db", { verbose: console.log });
 
-// todo: uType
 db.exec(`
-	CREATE TABLE IF NOT EXISTS users (
+	CREATE TABLE IF NOT EXISTS Users (
 		uid INTEGER PRIMARY KEY, 
 		username TEXT NOT NULL UNIQUE, 
 		uemail TEXT NOT NULL UNIQUE, 
@@ -15,7 +14,44 @@ db.exec(`
 		ufirstname TEXT,
 		ulastname TEXT,
 		ucity TEXT,
-		urating REAL DEFAULT 0
+		cid INTEGER REFERENCES Contractors(cid)
+	);
+
+	CREATE TABLE IF NOT EXISTS Contractors (
+		cid INTEGER PRIMARY KEY,
+		uid INTEGER REFERENCES Users(uid),
+		jobType TEXT NOT NULL,
+		biography TEXT,
+		rating REAL DEFAULT 0
+	);
+
+	CREATE TABLE IF NOT EXISTS JobRequests (
+		rid INTEGER PRIMARY KEY,
+		jid INTEGER REFERENCES ContractorJobs(jid),
+		uid INTEGER REFERENCES Users(uid),
+		offeredPrice INTEGER DEFAULT 0,
+		datePosted TEXT DEFAULT CURRENT_TIMESTAMP,
+		status INTEGER DEFAULT 0
+	);
+
+	CREATE TABLE IF NOT EXISTS ContractorJobs (
+		jid INTEGER PRIMARY KEY,
+		cid INTEGER REFERENCES Contractors(cid),
+		jobTitle TEXT,
+		jobDescription TEXT,
+		location TEXT,
+		startingPrice INTEGER DEFAULT 0,
+		datePosted TEXT DEFAULT CURRENT_TIMESTAMP,
+		status INTEGER DEFAULT 0
+	);
+
+	CREATE TABLE IF NOT EXISTS Ratings (
+		ratingid INTEGER PRIMARY KEY,
+		reviewerId INTEGER REFERENCES Users(uid),
+		cid INTEGER REFERENCES Contractors(cid),
+		rid INTEGER REFERENCES JobRequests(rid),
+		rating INTEGER DEFAULT 0,
+		review TEXT
 	);
 `);
 
