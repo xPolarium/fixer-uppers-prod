931cc36594f19e9469a5c4ee1d05c2f93be7e26a
Polarium <mikeajr@icloud>
2025-05-09 20:19:04 -0500

    fixed pages

--- src/app/reviews/editreviews.js
+++
@@ -1,3 +0,0 @@
-export default function EditReviews() {
-	return <div>Not yet implemented :C</div>;
-}
--- src/app/user_profile/user_profile.js
+++ src/app/user_profile/user_profile.js
@@ -1,3 +1,6 @@
+"use client";
+import React from "react";
+
 export default function UserProfile() {
 	return <div>Not yet implemented :C</div>;
 }
